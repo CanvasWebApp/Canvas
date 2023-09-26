4b62d879297b15c683ab0caab49e06dae7496123
CanvasWebApp <146054055+CanvasWebApp@users.noreply.github.com>
2023-09-26 00:22:25 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://canvas.mjs-m.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YzgxNDMwY2E4MzU4MGQxMjM0NDc4ZWZlYTEzM2FkZiIsInN1YiI6IjY1MTI1OThiZTFmYWVkMDExZDVmYjk4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MKxDU3AH79hPMkcw__df4xtDHrgL9v-b9ai6CxCYNOA"
+};
